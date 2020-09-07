235552912bcad302b5de458a9f405242a6bc20c9
Sophh3ak <sounsopheak7@gmail.com>
2020-09-07 16:20:50 +0700

    Update web.php

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -22,6 +22,6 @@ Route::get('/contact', 'PagesController@contact')->name('contact');
 Route::get('/readMore', 'PagesController@readMore')->name('readMore');
 
 Route::get('/test', function (){
-    return 'Fuck off';
+    return 'haha wanna play';
 });
 
