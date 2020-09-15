2b72bc88b7770602837ea2a906c78459ec712095
Soph3ak <sounsopheak7@gmail.com>
2020-09-15 13:52:24 +0700

    office

--- blog/resources/views/admin/posts.blade.php
+++ blog/resources/views/admin/posts.blade.php
@@ -1,4 +1,5 @@
 @extends('master.master-admin')
+@section('title','All Posts')
 @section('style')
 @endsection
 @section('content')
@@ -8,7 +9,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0 text-dark">Posts</h1>
+                        <h1 class="m-0 text-dark">Posts List</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
@@ -34,28 +35,16 @@
                             <div class="card-body">
                                 <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
                                     <div class="row">
-                                        <div class="col-sm-12 col-md-12 d-flex justify-content-between">
-                                            <div class="dataTables_length" id="example1_length">
-                                                <label>Show
-                                                    <select name="example1_length" aria-controls="example1" class="custom-select custom-select-sm form-control form-control-sm">
-                                                        <option value="10">10</option>
-                                                        <option value="25">25</option>
-                                                        <option value="50">50</option>
-                                                        <option value="100">100</option>
-                                                    </select> entries
-                                                </label>
-                                            </div>
-
-                                            <div id="example1_filter" class="dataTables_filter">
-                                                <label>Search:
-                                                    <input type="search" class="form-control form-control-sm" placeholder="" aria-controls="example1">
-                                                </label>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row"><div class="col-sm-12"><table id="example1" class="table table-bordered table-striped dataTable dtr-inline" role="grid" aria-describedby="example1_info">
+                                        <div class="col-sm-12">
+                                            <table id="example1" class="table table-bordered table-striped dataTable dtr-inline" role="grid" aria-describedby="example1_info">
                                                 <thead class="table-success">
-                                                <tr role="row"><th class="sorting_asc" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending">Rendering engine</th><th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending">Browser</th><th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Platform(s)</th><th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending">Engine version</th><th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">CSS grade</th></tr>
+                                                    <tr role="row">
+                                                        <th class="sorting_asc" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending">Rendering engine</th>
+                                                        <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending">Browser</th>
+                                                        <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending">Platform(s)</th>
+                                                        <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending">Engine version</th>
+                                                        <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending">CSS grade</th>
+                                                    </tr>
                                                 </thead>
                                                 <tbody>
                                                 <tr role="row" class="odd">
@@ -64,103 +53,120 @@
                                                     <td>Win 98+ / OSX.2+</td>
                                                     <td>1.7</td>
                                                     <td>A</td>
-                                                </tr><tr role="row" class="even">
+                                                </tr>
+                                                <tr role="row" class="even">
                                                     <td tabindex="0" class="sorting_1">Gecko</td>
                                                     <td>Firefox 1.5</td>
                                                     <td>Win 98+ / OSX.2+</td>
                                                     <td>1.8</td>
                                                     <td>A</td>
-                                                </tr><tr role="row" class="odd">
+                                                </tr>
+                                                <tr role="row" class="odd">
                                                     <td tabindex="0" class="sorting_1">Gecko</td>
                                                     <td>Firefox 2.0</td>
                                                     <td>Win 98+ / OSX.2+</td>
                                                     <td>1.8</td>
                                                     <td>A</td>
-                                                </tr><tr role="row" class="even">
+                                                </tr>
+                                                <tr role="row" class="even">
                                                     <td tabindex="0" class="sorting_1">Gecko</td>
                                                     <td>Firefox 3.0</td>
                                                     <td>Win 2k+ / OSX.3+</td>
                                                     <td>1.9</td>
                                                     <td>A</td>
-                                                </tr><tr role="row" class="odd">
+                                                </tr>
+                                                <tr role="row" class="odd">
                                                     <td class="sorting_1" tabindex="0">Gecko</td>
                                                     <td>Camino 1.0</td>
                                                     <td>OSX.2+</td>
                                                     <td>1.8</td>
                                                     <td>A</td>
-                                                </tr><tr role="row" class="even">
+                                                </tr>
+                                                <tr role="row" class="even">
                                                     <td class="sorting_1" tabindex="0">Gecko</td>
                                                     <td>Camino 1.5</td>
                                                     <td>OSX.3+</td>
                                                     <td>1.8</td>
                                                     <td>A</td>
-                                                </tr><tr role="row" class="odd">
+                                                </tr>
+                                                <tr role="row" class="odd">
                                                     <td class="sorting_1" tabindex="0">Gecko</td>
                                                     <td>Netscape 7.2</td>
                                                     <td>Win 95+ / Mac OS 8.6-9.2</td>
                                                     <td>1.7</td>
                                                     <td>A</td>
-                                                </tr><tr role="row" class="even">
+                                                </tr>
+                                                <tr role="row" class="even">
                                                     <td class="sorting_1" tabindex="0">Gecko</td>
                                                     <td>Netscape Browser 8</td>
                                                     <td>Win 98SE+</td>
                                                     <td>1.7</td>
                                                     <td>A</td>
-                                                </tr><tr role="row" class="odd">
+                                                </tr>
+                                                <tr role="row" class="odd">
                                                     <td class="sorting_1" tabindex="0">Gecko</td>
                                                     <td>Netscape Navigator 9</td>
                                                     <td>Win 98+ / OSX.2+</td>
                                                     <td>1.8</td>
                                                     <td>A</td>
-                                                </tr><tr role="row" class="even">
+                                                </tr>
+                                                <tr role="row" class="even">
                                                     <td class="sorting_1" tabindex="0">Gecko</td>
                                                     <td>Mozilla 1.0</td>
                                                     <td>Win 95+ / OSX.1+</td>
                                                     <td>1</td>
                                                     <td>A</td>
-                                                </tr><tr role="row" class="odd">
+                                                </tr>
+                                                <tr role="row" class="odd">
                                                     <td class="sorting_1" tabindex="0">Gecko</td>
                                                     <td>Mozilla 1.1</td>
                                                     <td>Win 95+ / OSX.1+</td>
                                                     <td>1.1</td>
                                                     <td>A</td>
-                                                </tr><tr role="row" class="even">
+                                                </tr>
+                                                <tr role="row" class="even">
                                                     <td class="sorting_1" tabindex="0">Gecko</td>
                                                     <td>Mozilla 1.2</td>
                                                     <td>Win 95+ / OSX.1+</td>
                                                     <td>1.2</td>
                                                     <td>A</td>
-                                                </tr><tr role="row" class="odd">
+                                                </tr>
+                                                <tr role="row" class="odd">
                                                     <td class="sorting_1" tabindex="0">Gecko</td>
                                                     <td>Mozilla 1.3</td>
                                                     <td>Win 95+ / OSX.1+</td>
                                                     <td>1.3</td>
                                                     <td>A</td>
-                                                </tr><tr role="row" class="even">
+                                                </tr>
+                                                <tr role="row" class="even">
                                                     <td class="sorting_1" tabindex="0">Gecko</td>
                                                     <td>Mozilla 1.4</td>
                                                     <td>Win 95+ / OSX.1+</td>
                                                     <td>1.4</td>
                                                     <td>A</td>
-                                                </tr><tr role="row" class="odd">
+                                                </tr>
+                                                <tr role="row" class="odd">
                                                     <td class="sorting_1" tabindex="0">Gecko</td>
                                                     <td>Mozilla 1.5</td>
                                                     <td>Win 95+ / OSX.1+</td>
                                                     <td>1.5</td>
                                                     <td>A</td>
-                                                </tr><tr role="row" class="even">
+                                                </tr>
+                                                <tr role="row" class="even">
                                                     <td class="sorting_1" tabindex="0">Gecko</td>
                                                     <td>Mozilla 1.6</td>
                                                     <td>Win 95+ / OSX.1+</td>
                                                     <td>1.6</td>
                                                     <td>A</td>
-                                                </tr><tr role="row" class="odd">
+                                                </tr>
+                                                <tr role="row" class="odd">
                                                     <td class="sorting_1" tabindex="0">Gecko</td>
                                                     <td>Mozilla 1.7</td>
                                                     <td>Win 98+ / OSX.1+</td>
                                                     <td>1.7</td>
                                                     <td>A</td>
-                                                </tr><tr role="row" class="even">
+                                                </tr>
+                                                <tr role="row" class="even">
                                                     <td class="sorting_1" tabindex="0">Gecko</td>
                                                     <td>Mozilla 1.8</td>
                                                     <td>Win 98+ / OSX.1+</td>
@@ -408,11 +414,20 @@
                                                     <td>S60</td>
                                                     <td>413</td>
                                                     <td>A</td>
-                                                </tr></tbody>
+                                                </tr>
+                                                </tbody>
                                                 <tfoot>
-                                                <tr><th rowspan="1" colspan="1">Rendering engine</th><th rowspan="1" colspan="1">Browser</th><th rowspan="1" colspan="1">Platform(s)</th><th rowspan="1" colspan="1">Engine version</th><th rowspan="1" colspan="1">CSS grade</th></tr>
+                                                <tr>
+                                                    <th rowspan="1" colspan="1">Rendering engine</th>
+                                                    <th rowspan="1" colspan="1">Browser</th>
+                                                    <th rowspan="1" colspan="1">Platform(s)</th>
+                                                    <th rowspan="1" colspan="1">Engine version</th>
+                                                    <th rowspan="1" colspan="1">CSS grade</th>
+                                                </tr>
                                                 </tfoot>
-                                            </table></div></div><div class="row"><div class="col-sm-12 col-md-5"><div class="dataTables_info" id="example1_info" role="status" aria-live="polite">Showing 1 to 57 of 57 entries</div></div><div class="col-sm-12 col-md-7"><div class="dataTables_paginate paging_simple_numbers" id="example1_paginate"><ul class="pagination"><li class="paginate_button page-item previous disabled" id="example1_previous"><a href="#" aria-controls="example1" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li><li class="paginate_button page-item active"><a href="#" aria-controls="example1" data-dt-idx="1" tabindex="0" class="page-link">1</a></li><li class="paginate_button page-item next disabled" id="example1_next"><a href="#" aria-controls="example1" data-dt-idx="2" tabindex="0" class="page-link">Next</a></li></ul></div></div></div></div>
+                                            </table></div>
+                                    </div>
+                                </div>
                             </div>
                             <!-- /.card-body -->
                         </div>
